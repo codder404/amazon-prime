07849cea22e960477f8e6510ab48f1dafcd229a1
fernando <nando4803@gmail.com>
2019-07-31 15:12:32 +0100

    Criando a interface do Amazon-prime com Vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header/Header';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: 0;
+    }
+
+    html, body, #root {
+        height: 100%;
+        width: 100%;
+    }
+
+    body {
+        text-rendering: optimizeLegibility !important;
+        -webkit-font-smoothing: antialised !important;
+        background: #0F171E;
+        font-family: Arial, Helvetica, sans-serif;
+        color: #FFF;
+    }
 </style>
